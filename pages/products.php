791d28471e11b183a3d143e26da42716e083489b
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-07-30 23:26:49 -0700

    products page static design

--- pages/products.php
+++ pages/products.php
@@ -1,9 +1,15 @@
+<?php
+  $category = explode("-", $c);
+  $category = ucwords($category[0])." - ".ucwords($category[1]);
+?>
 
 <!-- main page part -->
 <main>
   <div class="container-fluid container-products">
     <div class="row no-gutters">
       <aside class="sidebar-filter">
+        <p class="h3 mt-3"><?= $category; ?></p>
+        <hr>
         <form class="filter-form" method="get">
           <div class="accordion">
             <div class="card">
@@ -12,9 +18,12 @@
               </div>
             </div>
             <div class="collapse" id="sort-by">
-              <input type="radio" name="sort" value="low_to_high"> Price: Low To High<br/>
-              <input type="radio" name="sort" value="high_to_low"> Price: High To Low<br/>
-              <input type="radio" name="sort" value="newest"> Newest
+              <input type="radio" name="sort" id="low_to_high" value="low_to_high">
+              <label for="low_to_high">Price: Low To High</label><br/>
+              <input type="radio" name="sort" id="high_to_low" value="high_to_low">
+              <label for="high_to_low">Price: High To Low</label><br/>
+              <input type="radio" name="sort" id="newest" value="newest" checked>
+              <label for="newest">Newest</label>
             </div>
 
             <div class="card">
@@ -31,12 +40,106 @@
               <div id="slider-range"></div>
             </div>
 
+            <div class="card">
+              <div class="card-header collapsed" data-toggle="collapse" data-target="#size">
+                Size
+              </div>
+            </div>
+            <div class="collapse" id="size">
+              <input type="checkbox" name="size" value="XS" checked hidden>
+              <input type="checkbox" name="size" value="S" checked hidden>
+              <input type="checkbox" name="size" value="M" checked hidden>
+              <input type="checkbox" name="size" value="L" checked hidden>
+              <input type="checkbox" name="size" value="XL" checked hidden>
+              <input type="checkbox" name="size" value="XXL" checked hidden>
+              <div class="row no-gutters size-btn-group px-4">
+                <div class="col-4 text-center selected" data-select="XS">
+                  XS
+                </div>
+                <div class="col-4 text-center selected" data-select="S">
+                  S
+                </div>
+                <div class="col-4 text-center selected" data-select="M">
+                  M
+                </div>
+                <div class="col-4 text-center selected" data-select="L">
+                  L
+                </div>
+                <div class="col-4 text-center selected" data-select="XL">
+                  XL
+                </div>
+                <div class="col-4 text-center selected" data-select="XXL">
+                  XXL
+                </div>
+
+              </div>
+            </div>
+
 
           </div>
         </form>
       </aside>
+
       <div class="col item-list">
-        hi
+        <div class="row justify-content-start">
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+          <div class="col-12 col-sm-6 col-md-4 col-lg-3 text-center product-item">
+            <p class="h5">Men's Polo Shirts</p>
+            <img class="img-fluid" src="/imgs/products/polo_shirts.jpg" alt="polo_shirts.jpg">
+            <p class="price">$20.99</p>
+            <a href="/product/1" class="btn btn-sm btn-success">Details</a>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
--- script/script.js
+++ script/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+// Some global variables such as deviceWidth, scrollPosition, etc.
+  var deviceWidth = $(window).width() + 15;
+  var scrollPosition = $(window).scrollTop();
+  // For development purpose
+  $(window).on('resize', function() {
+    deviceWidth = $(window).width() + 15;
+    // console.log(deviceWidth);
+  })
+  $(window).on('scroll', function() {
+    scrollPosition = $(window).scrollTop();
+    // console.log(scrollPosition);
+  })
+
 // Navbar behaviors ****************************************
   // Products menu dropdown
     $(".product-dropdown-button").click(function(e) {
@@ -16,23 +29,19 @@ $(document).ready(function(){
 
   // Lower navbar fixed on scroll down
   // works only for desktop and tablet view
-  var deviceWidth = $(window).width();
-  if(deviceWidth >= 465) {
-    var scrollPosition = $(window).scrollTop();
-      $(window).scroll(function() {
-        scrollPosition = $(window).scrollTop();
-        if(scrollPosition >= 80) {
-          // change lower navbar to fixed
-          $('.lower-nav').addClass("fixed");
-        } else {
-          // change lower navbar to normal
-          $('.lower-nav').removeClass("fixed");
-        }
-      })
+  if(deviceWidth >= 480) {
+    $(window).scroll(function() {
+      scrollPosition = $(window).scrollTop();
+      if(scrollPosition >= 80) {
+        // change lower navbar to fixed
+        $('.lower-nav').addClass("fixed");
+      } else {
+        // change lower navbar to normal
+        $('.lower-nav').removeClass("fixed");
+      }
+    })
   }
 
-  // For development purpose
-  console.log(deviceWidth);
 
 
 // Home page ******************************************|
@@ -49,15 +58,31 @@ $(document).ready(function(){
 });
 
 // Products **************************************************/
-
+var minPrice;
+var maxPrice;
 $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 500,
+      max: 1000,
       values: [ 75, 300 ],
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        minPrice = ui.values[0];
+        maxPrice = ui.values[1];
+        // console.log(minPrice+" - "+maxPrice);
       }
     });
     $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+minPrice = $('#slider-range').slider("values", 0);
+maxPrice = $('#slider-range').slider("values", 1);
+// console.log(minPrice+" - "+maxPrice);
+
+// size btns
+$('.size-btn-group>.col-4').on('click', function() {
+  var selectedSize = $(this).data("select");
+  $(this).toggleClass("selected");
+  $('#size>input[value='+selectedSize+']').prop("checked")?
+  $('#size>input[value='+selectedSize+']').prop("checked", false) :
+  $('#size>input[value='+selectedSize+']').prop("checked", true);
+});
